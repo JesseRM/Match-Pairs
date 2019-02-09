0a779e417c6bdad75f4817fae94953b469d344a3
JesseRM <jesserrm@gmail.com>
2019-02-08 23:19:18 -0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
+app.use('/', require('./routes/routes'));
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log('Listening for requests');
+});
