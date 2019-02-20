c504b21ccc6ddc668d8e560b95fd68f46017cc05
JesseRM <jesserrm@gmail.com>
2019-02-19 23:02:30 -0800

    Create getValues method for Deck class

--- public/src/js/deck.js
+++ public/src/js/deck.js
@@ -1,3 +1,5 @@
+import {words} from './words'
+
 class Deck {
     constructor(type, grid) {
         this.type = type;
@@ -9,15 +11,21 @@ class Deck {
     }
 
     setCards(canvas) {
-        let values = getValues(this.type);
+        let values = this.getValues(this.type, this.size);
         
         for (let i = 0; i < values.length; i++) {
             let coordinates = [];
 
             if (this.cards.length === 0) {
-                coordinates = [5, 5]
+                coordinates = [this.cardPadding, this.cardPadding];
             } else {
-                coordinates = 
+                coordinates[0] = this.cards[i - 1]['coordinates'][0] + this.cardWidth + this.cardPadding;
+                coordinates[1] = this.cards[i - 1]['coordinates'][1];
+
+                if (coordinates[0] > canvas.width) {
+                    coordinates[0] = this.cardPadding;
+                    coordinates[1] = this.cards[i - 1]['coordinates'][1] + this.cardHeight + this.cardPadding;
+                }
             }
             
             this.cards.push({
@@ -25,9 +33,35 @@ class Deck {
                 value: values[i],
                 width: this.cardWidth,
                 height: this.cardHeight,
-                coordinates
+                coordinates: [coordinates[0], coordinates[1]]
             })
         }
 
+    }
+
+    getValues(type, size) {
+        let values = [];
+
+        if (type === 'number') {
+            while (values.length < size) {
+                let rand = Math.floor(Math.random() * (100 - 1) + 1);
+                
+                if (values.includes(rand)) continue;
+
+                values.push(rand, rand);
+            }
+        }
+
+        if (type === 'word') {
+            while (values.length < size) {
+                let rand = Math.floor(Math.random() * (size + 1));
+
+                if (values.includes(words[rand])) continue;
+
+                values.push(words[rand], words[rand]);
+            }
+        }
+
+        return values;
     }
 }
