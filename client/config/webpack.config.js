d85bc2860ea3c6aacdd2411b7585a325ebbaee49
JesseRM <jesserrm@gmail.com>
2021-09-02 23:22:11 -0700

    Change folder structure

--- client/config/webpack.config.js
+++ client/config/webpack.config.js
@@ -2,9 +2,9 @@ const path = require('path');
 
 module.exports = {
     mode: 'production',
-    entry: './public/src/js/matchpairs.js',
+    entry: './client/src/js/matchpairs.js',
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, 'public/dist/js')
+        path: path.resolve(__dirname, '../public/dist/js')
     }
 };
