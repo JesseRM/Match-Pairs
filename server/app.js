bd73875dfae696c84837466c6535f42abe6c214b
JesseRM <jesserrm@gmail.com>
2021-09-02 23:25:56 -0700

    Adjust path of static files folder

--- server/app.js
+++ server/app.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
-app.use(express.static('public/dist'));
+app.use(express.static(path.resolve(__dirname, '../client/public')));
 app.use('/', require('./routes/routes'));
 
 app.listen(process.env.PORT || 3000, () => {
