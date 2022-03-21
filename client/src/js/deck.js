535523ac616d4368f758916628d81efcca0fa713
JesseRM <jesserrm@gmail.com>
2022-03-21 00:36:23 -0700

    Make adjustments for clarity, decoupling

--- client/src/js/deck.js
+++ client/src/js/deck.js
@@ -1,147 +1,149 @@
 class Deck {
-    constructor(type, grid, canvas) {
-        this.type = type;
-        this.size = grid[0] * grid[1];
-        this.possibleVals = null;
-        this.cards = [];
-        this.matched = 0;
-        this.cardWidth = canvas.element.width / grid[0];
-        this.cardHeight = canvas.element.height / grid[1]; 
+  constructor(type, size, cardWidth, cardHeight) {
+    this.type = type;
+    this.size = size;
+    this.possibleVals = null;
+    this.cards = [];
+    this.matched = 0;
+    this.cardWidth = cardWidth;
+    this.cardHeight = cardHeight;
+    this.numStart = 1;
+    this.numEnd = 100; 
+  }
+
+  setPossibleVals(values) {
+    this.possibleVals = values;
+  }
+
+  setCards(values, gameBoardWidth) {
+    const vals = this.randomizeVals(values);
+    let posX = 0; 
+    let posY = 0;
+    
+    for (let i = 0; i < vals.length; i++) {
+      this.cards.push({
+        cardNum: i,
+        type: this.type,
+        value: vals[i],
+        width: this.cardWidth,
+        height: this.cardHeight,
+        matched: false,
+        x: posX,
+        y: posY
+      });
+
+      posX = posX + this.cardWidth;
+
+      //Check if we need to start a new row
+      if (posX >= gameBoardWidth) {
+        posX = 0;
+        posY = posY + this.cardHeight;
+      }
     }
+  }
 
-    setPossibleVals(values) {
-        this.possibleVals = values;
-    }
-
-    setCards(values, canvas) {
-        let vals = this.randomizeVals([...values]);
-        let posX = 0, posY = 0;
-        
-        for (let i = 0; i < vals.length; i++) {
-            this.cards.push({
-                cardNum: i,
-                type: this.type,
-                value: vals[i],
-                width: this.cardWidth,
-                height: this.cardHeight,
-                matched: false,
-                x: posX,
-                y: posY
-            });
-
-            posX = posX + this.cardWidth;
-
-            //Check if we need to start a new row
-            if (posX >= canvas.element.width) {
-                posX = 0;
-                posY = posY + this.cardHeight;
-            }
-        }
-    }
-
-    getValues() {
-        return new Promise((resolve) => {
-            if (this.type === 'word') resolve(this.getWords());
-            if (this.type === 'number') resolve(this.getNumbers(1, 100));
-            if (this.type === 'picture') {
-                this.getImages().then((images) => {
-                    resolve(images);
-                });
-            }
-        });   
-    }
-
-    getNumbers(min, max) {
-        let nums = [];
-
-        while (nums.length < this.size) {
-            let rand = String(Math.floor(Math.random() * (max - min + 1) + min));
-            
-            if (nums.includes(rand)) continue;
-
-            nums.push(rand, rand);
-        }
-
-        return nums;
-    }
-
-    getWords() {
-        let words = [];
-        
-        while (words.length < this.size) {
-            let rand = Math.floor(Math.random() * (this.possibleVals.length));
-
-            if (words.includes(this.possibleVals[rand])) continue;
-
-            words.push(this.possibleVals[rand], this.possibleVals[rand]);
-        }
-
-        return words;
-    }
-
-    async getImages() {
-        let imgNames = this.getImageNames();
-        let imgs = [];
-
-        for (let i = 0; i < imgNames.length; i++) {
-            let img = await this.loadImage(imgNames[i]);
-        
-            imgs.push(img, img);
-        }
-
-        return imgs;
-    }
-
-    getImageNames() {
-        let names = [];
-
-        while (names.length < this.size / 2) {
-            let rand = Math.floor(Math.random() * (this.possibleVals.length));
-
-            if (names.includes(this.possibleVals[rand])) continue;
-
-            names.push(this.possibleVals[rand]);
-        }
-
-        return names;
-    }
-
-    loadImage(imgName) {
-        return new Promise((resolve, reject) => {
-            let img = new Image();
-            img.src = `./images/${imgName}.png`;
-            
-            img.onload = () => {
-                console.log('loaded');
-                resolve(img);
-            }
-
-            img.onerror = () => {
-                console.log('Error loading image');
-            }
+  getValues() {
+    return new Promise((resolve) => {
+      if (this.type === "word") resolve(this.getWords());
+      if (this.type === "number") resolve(this.getNumbers(this.numStart, this.numEnd));
+      if (this.type === "picture") {
+        this.getImages().then((images) => {
+          resolve(images);
         });
+      }
+    });   
+  }
+
+  getNumbers(min, max) {
+    const nums = [];
+
+    while (nums.length < this.size) {
+      const randNum = String(Math.floor(Math.random() * (max - min + 1) + min));
+      
+      if (nums.includes(randNum)) continue;
+
+      nums.push(randNum, randNum);
     }
 
-    getClickedCard(coor) {
-        for (let i = 0; i < this.cards.length; i++) {
-            if (coor.x >= this.cards[i].x && coor.x <= (this.cards[i].x + this.cardWidth)
-                && coor.y >= this.cards[i].y && coor.y <= (this.cards[i].y + this.cardHeight)) {
-                    return this.cards[i];
-                }
-        }
+    return nums;
+  }
+
+  getWords() {
+    const words = [];
+    
+    while (words.length < this.size) {
+      const randNum = Math.floor(Math.random() * (this.possibleVals.length));
+
+      if (words.includes(this.possibleVals[randNum])) continue;
+
+      words.push(this.possibleVals[randNum], this.possibleVals[randNum]);
     }
 
-    randomizeVals(values) {
-        let vals = [...values];
-        
-        for (let i = vals.length - 1; i > 0; i--) {
-            let j = Math.floor(Math.random() * (i + 1));
+    return words;
+  }
 
-            [vals[i], vals[j]] = [vals[j], vals[i]];
-        }
+  async getImages() {
+    const imgNames = this.getImageNames();
+    const imgs = [];
 
-        return vals;
+    for (let imgName of imgNames) {
+      const img = await this.loadImage(imgName);
+
+      imgs.push(img, img);
     }
+
+    return imgs;
+  }
+
+  getImageNames() {
+    const names = [];
+
+    while (names.length < this.size / 2) {
+      const randNum = Math.floor(Math.random() * (this.possibleVals.length));
+
+      if (names.includes(this.possibleVals[randNum])) continue;
+
+      names.push(this.possibleVals[randNum]);
+    }
+
+    return names;
+  }
+
+  loadImage(imgName) {
+    return new Promise((resolve, reject) => {
+      const img = new Image();
+      img.src = `./images/${imgName}.png`;
+      
+      img.onload = () => {
+        resolve(img);
+      }
+
+      img.onerror = () => {
+        console.log("Error loading image " + imgName);
+      }
+    });
+  }
+
+  getClickedCard(coor) {
+    for (let i = 0; i < this.cards.length; i++) {
+      if (coor.x >= this.cards[i].x && coor.x <= (this.cards[i].x + this.cardWidth)
+        && coor.y >= this.cards[i].y && coor.y <= (this.cards[i].y + this.cardHeight)) {
+        return this.cards[i];
+      }
+    }
+  }
+
+  randomizeVals(values) {
+    const vals = [...values];
+    
+    for (let i = vals.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+
+      [vals[i], vals[j]] = [vals[j], vals[i]];
+    }
+
+    return vals;
+  }
 }
 
-export {Deck}
+export default Deck;
