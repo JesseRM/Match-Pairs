10d3e91a6c07e2801e24aef65d59c91cd24800ea
JesseRM <jesserrm@gmail.com>
2022-03-28 10:32:44 -0700

    Adjust spacing

--- client/config/webpack.config.js
+++ client/config/webpack.config.js
@@ -1,10 +1,10 @@
 const path = require('path');
 
 module.exports = {
-    mode: 'production',
-    entry: './client/src/js/matchpairs.js',
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, '../public/dist/js')
-    }
+  mode: 'production',
+  entry: './client/src/js/matchpairs.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, '../public/dist/js')
+  }
 };
